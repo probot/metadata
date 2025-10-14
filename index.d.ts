ae17c3cbc431d58adb7327a30b4e86e3132fe30a
Aras Abbasi <aras.abbasi@googlemail.com>
2025-10-14 14:31:24 +0200

    proper type generation

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,16 @@
-declare module "index" {
-    export default metadata;
-    export type StringOrNumber = string | number;
-    export type Key = Record<string, StringOrNumber> | StringOrNumber | StringOrNumber[];
-    export type Value = Key;
-    export type IssueOption = {
-        owner: string;
-        repo: string;
-        issue_number: number;
-        body?: string | undefined;
-    };
-    export type ProbotMetadata = {
-        get: (key?: Key) => Promise<Value | undefined>;
-        set: (key?: Key, value?: Value) => Promise<void>;
-    };
-    export type ProbotMetadataConstructor = (context: import("probot").Context<"issue_comment">, issue?: IssueOption) => ProbotMetadata;
-    export const metadata: ProbotMetadataConstructor;
-}
+export default metadata;
+export type StringOrNumber = string | number;
+export type Key = Record<string, StringOrNumber> | StringOrNumber | StringOrNumber[];
+export type Value = Key;
+export type IssueOption = {
+    owner: string;
+    repo: string;
+    issue_number: number;
+    body?: string;
+};
+export type ProbotMetadata = {
+    get: (key?: Key) => Promise<Value | undefined>;
+    set: (key?: Key, value?: Value) => Promise<void>;
+};
+export type ProbotMetadataConstructor = (context: import("probot").Context<"issue_comment">, issue?: IssueOption) => ProbotMetadata;
+export const metadata: ProbotMetadataConstructor;
